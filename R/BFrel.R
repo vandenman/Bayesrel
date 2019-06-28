4aed4d31e20f0b201c99a7b15eaa7661c9531f0a
juliuspf <38500953+juliuspf@users.noreply.github.com>
2019-06-28 11:34:00 +0200

    Delete BFrel.R

--- R/BFrel.R
+++
@@ -1,8 +0,0 @@
-
-#' gives the odds ratio of an estimate being bigger than low.bound between the prior and the posterior
-#' needs a posterior and a prior sample of the estimate as input, and the critical value of the hypothesis
-#'
-#' @export
-bfrel <- function(xprior, xpost, low.bound){
-  probrel(xprior, low.bound)/probrel(xpost, low.bound)
-}
