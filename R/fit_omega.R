313686b32628f7c6be1e33313abef8c2524ce5ec
Julius <julius.pfadt@gmail.com>
2019-07-05 12:19:38 +0200

    change in plot ppc

--- R/fit_omega.R
+++ R/fit_omega.R
@@ -27,19 +27,23 @@ fit_omega <- function(x){
     dtmp <- MASS::mvrnorm(nrow(x$data), rep(0, ncol(sigma)), cimpl)
     ee_impl[i, ] <- eigen(cov(dtmp))$values
   }
+  qq_ee_low <- apply(ee_impl, 2, quantile, prob = .025)
+  qq_ee_up <- apply(ee_impl, 2, quantile, prob = .975)
 
   plot(eigen(sigma)$values, axes = F, ylim = c(0, ymax), ylab = "Eigenvalue - Size", xlab = "Eigenvalue - No.")
+  lines(qq_ee_low, col = "gray50", lty = 2)
+  lines(qq_ee_up, col = "gray50", lty = 2)
+  polygon(x = c(1:ncol(sigma), rev(1:ncol(sigma))), y = c(qq_ee_up, rev(qq_ee_low)),
+          col = adjustcolor("gray", alpha.f = .7), border = NA)
+  lines(eigen(sigma)$values, type = "l", lwd = 2)
+  lines(eigen(sigma)$values, type = "p")
   axis(side = 1, at = seq(1:ncol(sigma)))
   axis(side = 2)
   title(main = "Posterior Predictive Check for Omega 1-Factor-Model")
 
-  apply(ee_impl, 1, lines, col = "gray")
-
-  lines(eigen(sigma)$values, type = "p")
-  lines(eigen(sigma)$values, type = "l", lwd = 2)
-  legend(ncol(sigma)/3, ymax*(2/3),
+  legend(ncol(sigma)/3*1.1, ymax*(2/3),
          legend = c("Dataset Covariance Matrix", "Simulated Data from Model Implied Covariance Matrix"),
-         col=c("black", "gray"), lwd = c(2, 2), box.lwd = 0)
+         col=c("black", "gray"), lwd = c(2, 2), box.lwd = 0, lty = c(1, 2), cex = .8)
 
 }
 
