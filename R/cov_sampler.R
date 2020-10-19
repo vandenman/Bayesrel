f83888512afa8307f6a4f30ebe9dfcd5f501a081
Julius Pfadt <julius.pfadt@gmail.com>
2020-10-19 15:39:51 +0200

    cleaning

--- R/cov_sampler.R
+++ R/cov_sampler.R
@@ -121,34 +121,3 @@ rinvwishart2 <- function(nu, S, k = nrow(S), dfChisq = nu:(nu-k+1), utz = upper.
   # LaplacesDemon::rinvwishart
   return(chol2inv(Z %*% S))
 }
-
-
-# covSamp_old <- function(data, n.iter, n.burnin){
-#   n <- nrow(data)
-#   p <- ncol(data)
-#   # posterior covariance matrix ---------------------------------------------------
-#   k0 <- 1e-10
-#   v0 <- p
-#   t <- diag(p)
-#   T0 <- solve(t/k0) # inverse scale matrix, prior
-#   mu0 <- rep(0, p) # prior means
-#   ym <- apply(data, 2, mean)
-#   # https://en.wikipedia.org/wiki/Normal-inverse-Wishart_distribution, murphy 2007
-#   mun <- (k0 * mu0 + n * ym) / (k0 + n)
-#   kn <- k0 + n
-#   vn <- v0 + n
-#   S <- 0
-  # for (i in 1:n){
-  #   M <- (data[i, ] - ym) %*% t(data[i, ] - ym)
-  #   S <- S + M
-  # }
-#   Tn <- T0 + S + (k0 * n / (k0 + n)) * (ym - mu0) %*% t(ym - mu0)
-#   # drawing samples from posterior:
-#   c_post <- array(0, c(n.iter, p, p))
-#   for ( i in 1:n.iter){
-#     c_post[i, , ] <- LaplacesDemon::rinvwishart(vn, Tn) # sample from inverse wishart
-#   }
-#   c_post <- c_post[(n.burnin + 1):n.iter, , ]
-#
-#   return(c_post)
-# }
